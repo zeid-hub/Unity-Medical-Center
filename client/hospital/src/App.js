d794fd56dd518da48ce4b0f199df6b90a21bf7be
abdishukri <hillow15521@gmail.com>
2024-04-18 16:03:35 +0300

    Done

--- client/hospital/src/App.js
+++ client/hospital/src/App.js
@@ -1,10 +1,21 @@
+import './App.css';
+import React from 'react';
+import About from './components/About'
+import Nurses from './components/Nurses'
+import { Route, Routes } from 'react-router-dom';
+
+
+
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/nurses" element={<Nurses />} />
+        </Routes>
+      
     </div>
   );
 }
-
 
 export default App;
